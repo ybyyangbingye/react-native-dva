c6e45297bfaef3c1d65815e4d3f4b8e8e2806aed
unknown <wangsisi@cn.net.ntes>
2018-07-16 10:31:32 +0800

    登陆页面

--- app/containers/Login.js
+++ app/containers/Login.js
@@ -6,58 +6,124 @@ import { Button, Touchable } from '../components'
 
 import { createAction, NavigationActions } from '../utils'
 
+import { ScrollView } from 'react-native';
+
+import { List, InputItem,Toast } from 'antd-mobile-rn';
+
 @connect(({ app }) => ({ ...app }))
 class Login extends Component {
   static navigationOptions = {
     title: 'Login',
   }
-
+  state = {
+    hasError: false,
+    value: '',
+    code:'',
+    codeTime:60
+  }
   onLogin = () => {
-    this.props.dispatch(createAction('app/login')())
+    Toast.info("hello");
+    //this.props.dispatch(createAction('app/login')())
+  }
+   onErrorClick = () => {
+    if (this.state.hasError) {
+      Toast.info('Please enter 11 digits');
+    }
   }
 
   onClose = () => {
     this.props.dispatch(NavigationActions.back())
   }
 
+/*  let phoneReg = '/^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/'*/
+  getCode=(value)=>{
+    let val = this.state.value.replace(/\s/g, '');
+    const phoneReg = /^[1][3,4,5,7,8][0-9]{9}$/;
+    if(!phoneReg.test(val)){
+      Toast.info("请输入合法的电话号码");
+    }else{
+      
+    }
+  }
+  onChange = (value) => {
+    if (value.replace(/\s/g, '').length < 11) {
+      this.setState({
+        hasError: true,
+      });
+    } else {
+      this.setState({
+        hasError: false,
+      });
+    }
+    this.setState({
+      value,
+    });
+  }
+
   render() {
-    const { fetching } = this.props
     return (
-      <View style={styles.container}>
-        {fetching ? (
-          <ActivityIndicator />
-        ) : (
-          <Button text="Login" onPress={this.onLogin} />
-        )}
-        {!fetching && (
-          <Touchable style={styles.close} onPress={this.onClose}>
-            <Image
-              style={styles.icon}
-              source={require('../images/close.png')}
+      <ScrollView >
+       <View >
+            <List  renderHeader={() => 'Hi 等的就是你'}>
+              <View style={{display:'flex',flexDirection:'row',alignItems:'center'}}>
+                <Image  style={styles.textIcon} source={require('../images/iphone.png')}/>
+                <View style={{flex:1}}>
+                <InputItem  
+                placeholder="请输入11位手机号码" 
+                type="phone" 
+                value={this.state.value}
+                onChange = {this.onChange}
+                error={this.state.hasError}
+                onErrorClick={this.onErrorClick}
+                />
+                </View>
+                <Button text="获取验证码" style={{width:110,marginRight:10}} onPress = {this.getCode} value={this.state.codeTime}/>
+              </View>
+               <View style={{display:'flex',flexDirection:'row',alignItems:'center'}}>
+                <Image  style={styles.textIcon} source={require('../images/code.png')}/>
+                <View style={{flex:1}}>
+                  <InputItem  placeholder="请输入短信验证码" style={{borderBottomWidth:0}}/>
+                </View>
+              </View>
+           </List>
+           <Button text="登录" onPress={this.onLogin} style={styles.button}/>
+        </View>
+       {/* <Touchable style={styles.close} onPress={this.onClose}>
+          <Image
+             style={styles.icon}
+             source={require('../images/close.png')}
             />
-          </Touchable>
-        )}
-      </View>
+        </Touchable>*/}
+        
+    </ScrollView>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  close: {
-    position: 'absolute',
-    right: 10,
-    top: 30,
-  },
-  icon: {
-    width: 24,
-    height: 24,
-    tintColor: 'gray',
-  },
-})
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    close: {
+      position: 'absolute',
+      right: 10,
+      top: 30,
+    },
+    icon: {
+      width: 24,
+      height: 24,
+      tintColor: 'gray',
+    },
+    textIcon:{
+      width:20,
+      height:20,
+      margin:20 
+    },
+    button:{
+      borderRadius:20,
+      margin:20
+    }
+  })
 
 export default Login
